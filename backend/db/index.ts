74c64abf9cc4fcf82b7b2178daa66a763796398f
Mahich123 <69717681+Mahich123@users.noreply.github.com>
2024-09-19 22:56:34 +0600

    fixing vercel db connection error

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+
+import pg from 'pg'
+const {Pool} = pg
 
 const pool = new Pool({
   connectionString: process.env.SUPABASE_DB_URL!,
